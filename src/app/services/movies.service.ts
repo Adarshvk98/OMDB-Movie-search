96e4444e03ce62452e149b6c0dfba9453f92ffc8
Adarshvk98 <adarshvk1997@yahoo.com>
2018-09-01 02:40:30 +0530

    updates to deploy to heroku

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Subject, ReplaySubject } from 'rxjs';
-import { map } from 'rxjs/Operators';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
