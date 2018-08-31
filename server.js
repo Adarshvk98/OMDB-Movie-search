2fa0aa1d1ae8af0a211e65adfbd9f008d528d13c
Adarshvk98 <adarshvk1997@yahoo.com>
2018-09-01 03:26:57 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ res.sendFile(path.join(__dirname,'/dist/movies/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000,'0.0.0.0');
