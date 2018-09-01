5fe1a1a83d3ada0f0da3bfa32c6ca15eb73897b5
Adarshvk98 <adarshvk1997@yahoo.com>
2018-09-01 06:59:09 +0530

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -12,4 +11,4 @@ res.sendFile(path.join(__dirname,'/dist/movies/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT ||5000);
+app.listen(process.env.PORT ||8080);
